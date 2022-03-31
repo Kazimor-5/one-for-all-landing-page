d4750fffe4b46bb9ecaf99fa5b8c1022046c503c
kahina-91 <kahinabenaouadi91@gmail.com>
2022-03-31 13:09:05 +0200

    slide_update

--- app.js
+++ app.js
@@ -1,23 +1,61 @@
-let slideIndex = 1;
+var slideIndex = 1;
 
-const plusSlide = (n) => {
-    showSlide(slideIndex += n);
+var myTimer;
+
+var slideshowContainer;
+
+window.addEventListener("load",function() {
+    showSlides(slideIndex);
+    myTimer = setInterval(function(){plusSlides(1)}, 3000);
+    slideshowContainer = document.getElementsByClassName('slideshow-inner')[0];
+  
+    slideshowContainer.addEventListener('mouseenter', pause)
+    slideshowContainer.addEventListener('mouseleave', resume)
+})
+
+
+function plusSlides(n){
+  clearInterval(myTimer);
+  if (n < 0){
+    showSlides(slideIndex -= 1);
+  } else {
+   showSlides(slideIndex += 1); 
+  }
+  
+  
+  if (n === -1){
+    myTimer = setInterval(function(){plusSlides(n + 2)}, 3000);
+  } else {
+    myTimer = setInterval(function(){plusSlides(n + 1)}, 3000);
+  }
 }
 
 
-const showSlide = (n) => {
-    let slides = document.getElementsByClassName("slide");
-    if (n > slides.length-1) {slideIndex = 0}    
-    if (n < 0) {slideIndex = slides.length-1}
-
-    for(let i = 0; i < slides.length-1; i++ ){
-        slides[i].style.display = "none";
-    }
-    slideIndex++;
-    console.log(slideIndex)
-    slides[slideIndex-1].style.display = "block";
-    
-    setTimeout(showSlide, 3000);
+function currentSlide(n){
+  clearInterval(myTimer);
+  myTimer = setInterval(function(){plusSlides(n + 1)}, 3000);
+  showSlides(slideIndex = n);
 }
 
-showSlide(slideIndex);
+function showSlides(n){
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+  }
+ 
+  slides[slideIndex-1].style.display = "block";
+ 
+}
+
+pause = () => {
+  clearInterval(myTimer);
+}
+
+resume = () =>{
+  clearInterval(myTimer);
+  myTimer = setInterval(function(){plusSlides(slideIndex)}, 3000);
+}
